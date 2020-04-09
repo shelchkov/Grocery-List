49c918c51f397a437dc72dc0c84fdd0c6e4ca865
shelchkovy <shelchkovy@yandex.ru>
2020-04-09 18:48:53 +0400

    Improve validation #44

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -17,12 +17,19 @@ interface SignInErrors {
 	[SignInInputs.password]?: string[]
 }
 
-const signInErrors: { [key: string]: string[] } = {
+const signInErrors: { 
+	[key: string]: string[] | (string | number)[][]
+} = {
 	required: [SignInInputs.email, SignInInputs.password],
+	minLength: [[SignInInputs.password, 4]]
 }
 
-const checkers: { [key: string]: (value?: string) => boolean } = {
-	required: (value?: string): boolean => !value
+const checkers: {
+	[key: string]: (value?: string, parameter?: number | string) => boolean
+} = {
+	required: (value?: string): boolean => !value,
+	minLength: (value?: string, parameter?: number | string): boolean =>
+		value && parameter && value.length < parameter || false
 }
 
 export const signInValidation = (
@@ -38,11 +45,17 @@ export const signInValidation = (
 	Object.keys(signInErrors).forEach((error: string): void => {
 		const fields = signInErrors[error]
 
-		fields.forEach((input: string): void => {
-			if (checkers[error](formData[input as SignInInputs])) {
-				errors[input as SignInInputs] = 
-				errors[input as SignInInputs] ? [
-					...(errors[input as SignInInputs] as string[]),
+		fields.forEach((input: string | (string | number)[]): void => {
+			const field = Array.isArray(input) ? input[0] : input
+			const parameters = Array.isArray(input) ? input[1] : undefined
+
+			if (checkers[error](
+				formData[field as SignInInputs],
+				parameters
+			)) {
+				errors[field as SignInInputs] = 
+				errors[field as SignInInputs] ? [
+					...(errors[field as SignInInputs] as string[]),
 					error
 				] : [ error ]
 			}
