3f3dca362fedd36aeaae23a332655bd55b03e468
shelchkovy <shelchkovy@yandex.ru>
2020-04-14 20:14:10 +0400

    Add loading logic to add new item form #52

--- src/components/add-new-item/add-new-item-buttons.tsx
+++ src/components/add-new-item/add-new-item-buttons.tsx
@@ -7,16 +7,19 @@ import { BtnTypes, ButtonTypes } from "../../utils/enums"
 interface Props {
 	isFormActive: boolean
 	setFormActive: () => void
+	isLoading: boolean
 }
 
 export const AddNewItemButtons = ({
 	isFormActive,
-	setFormActive
+	setFormActive,
+	isLoading,
 }: Props): ReactElement => isFormActive ? (
 	<Button
 		text="+"
 		buttonType={BtnTypes.Plus}
 		type={ButtonTypes.submit}
+		isLoading={isLoading}
 	/>
 ) : (
 	<Button
